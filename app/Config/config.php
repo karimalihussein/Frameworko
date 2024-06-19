c79d3d2a0f9ba6dbdb345a2a1a27b666129bd637
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-19 16:39:43 +0300

    Refactor Config class to allow merging of configuration arrays

--- app/Config/config.php
+++ app/Config/config.php
@@ -4,13 +4,16 @@ namespace App\Config;
 
 final class Config
 {
-    protected array $config = [
-        'app' => [
-            'name' => 'Frameworko',
-        ],
-    ];
+    protected array $config = [];
     public function get(string $key, $default = null)
     {
         return dot($this->config)->get($key, $default);
+    }
+
+    public function merge(array $config): self
+    {
+        $this->config = array_merge_recursive($this->config, $config);
+
+        return $this;
     }
 }
