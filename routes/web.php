7967ddbe253d72d86b6f86e9967d368104b02b3b
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-19 21:10:20 +0300

    Refactor routing in web.php to use a controller for the home page

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,7 @@ use League\Route\Router;
 use Psr\Container\ContainerInterface;
 
 return static function(Router $router, ContainerInterface $container) {
-    $router->map('GET', '/', function() {
-        $response = new \Laminas\Diactoros\Response();
-        $response->getBody()->write('Home');
-        return $response;
-    });
+    $router->map('GET', '/', \App\Http\Controllers\HomeController::class);
 
     $router->map('GET', '/about', function() {
         $response = new \Laminas\Diactoros\Response();
