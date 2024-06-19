2cfeaf0f44b3ac3d37a5c2ab3d7b0c840e5c646e
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-19 22:00:15 +0300

    Update HomeController to use Config class for app name in response

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Config\Config;
 use Laminas\Diactoros\Request;
 use Laminas\Diactoros\Response;
 use Psr\Http\Message\ServerRequestInterface;
 
 final class HomeController
 {
+    public function __construct(
+        protected Config $config
+    ){}
+
     public function __invoke(ServerRequestInterface $request): Response
     {
         $response = new Response();
-        $response->getBody()->write('Hello, World!');
+        $response->getBody()->write($this->config->get('app.name'));
         return $response;
     }
 }
