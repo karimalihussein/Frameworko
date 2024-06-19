b783104d9a81640b91c3203fbeac2541110d449b
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-19 20:57:30 +0300

    Refactor application bootstrap process and add Laminas Diactoros request handling in app.php

--- app/Core/App.php
+++ app/Core/App.php
@@ -3,10 +3,46 @@
 namespace App\Core;
 
 
-final class App 
+use Laminas\Diactoros\Request;
+use Laminas\Diactoros\Response;
+use League\Route\Router;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+final class App
 {
-    public function run()
+    protected  ServerRequestInterface $request;
+    protected Router $router;
+
+
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function __construct(protected ContainerInterface $container)
     {
-        
+        $this->request = $this->container->get(Request::class);
+        $this->router = $this->container->get(Router::class);
+    }
+
+    public function getRouter(): Router
+    {
+        return $this->router;
+    }
+
+    public function run(): void
+    {
+        $response = new Response();
+        try {
+            $response = $this->router->dispatch($this->request);
+        } catch (\Throwable $e) {
+            $response = new \Laminas\Diactoros\Response();
+            $response->getBody()->write('An error occurred');
+        }
+        (new \Laminas\HttpHandlerRunner\Emitter\SapiEmitter)->emit(
+            $response
+        );
     }
 }
