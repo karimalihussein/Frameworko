5cecc1af96b970c9a2373de639749d29f9414c4b
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-20 05:54:04 +0300

    Update HomeController to use View class for rendering the home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Config\Config;
+use App\Views\View;
 use Laminas\Diactoros\Request;
 use Laminas\Diactoros\Response;
 use Psr\Http\Message\ServerRequestInterface;
@@ -10,13 +11,16 @@ use Psr\Http\Message\ServerRequestInterface;
 final class HomeController
 {
     public function __construct(
-        protected Config $config
+        protected Config $config,
+        protected View $view
     ){}
 
     public function __invoke(ServerRequestInterface $request): Response
     {
         $response = new Response();
-        $response->getBody()->write($this->config->get('app.name'));
+        $response->getBody()->write($this->view->render('home', [
+            'name' => $this->config->get('app.name')
+        ]));
         return $response;
     }
 }
