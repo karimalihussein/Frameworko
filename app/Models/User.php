ab7d383fb8b6c7195872658f9099e36bc4d1d884
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-21 01:37:57 +0300

    Add User model to app/Models/User.php

---
+++ app/Models/User.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+final class User extends Model
+{
+    protected $table = 'users';
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public $timestamps = false;
+}
