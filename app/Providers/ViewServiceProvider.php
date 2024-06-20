377eba246fbc7745b19c8f7074943678114de234
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-20 06:08:09 +0300

    Refactor ViewServiceProvider to use Config class for debug mode in Twig environment

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Config\Config;
 use App\Views\View;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 use League\Container\ServiceProvider\BootableServiceProviderInterface;
@@ -17,10 +18,14 @@ final class ViewServiceProvider extends AbstractServiceProvider implements Boota
     {
         $this->getContainer()->add(View::class, function(){
             $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../../resources/views');
+            $isDebugEnabled = $this->getContainer()->get(Config::class)->get('app.debug');
             $twig = new \Twig\Environment($loader, [
                 'cache' => false,
-                'debug' => true
+                'debug' => $isDebugEnabled
             ]);
+
+            $twig->addExtension(new \Twig\Extension\DebugExtension());
+
             return new View($twig);
         })->setShared(true);
     }
