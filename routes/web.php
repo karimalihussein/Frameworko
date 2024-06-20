f9ff2d72a09259d2275791582dcb824a32ca16c0
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-21 01:52:08 +0300

    Add route for user profile page in web.php

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,5 @@ use Psr\Container\ContainerInterface;
 return static function(Router $router, ContainerInterface $container) {
     $router->map('GET', '/', \App\Http\Controllers\HomeController::class);
     $router->map('GET', '/about', \App\Http\Controllers\AboutController::class);
+    $router->map('GET', '/users/{user}', \App\Http\Controllers\UserController::class);
 };
