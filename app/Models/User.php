c2e8adfb4334d78e297b770a65ef9f2b0f6d4bee
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-21 20:18:22 +0300

    Refactor User model to include first name, last name, email, and password fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,18 @@
 
 namespace App\Models;
 
+use Cartalyst\Sentinel\Users\EloquentUser;
 use Illuminate\Database\Eloquent\Model;
 
-final class User extends Model
+final class User extends EloquentUser
 {
     protected $table = 'users';
 
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'email',
+        'password',
     ];
 
     public $timestamps = false;
