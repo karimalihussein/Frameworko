4f27b40e9de4cc57d9dba4fd78145b17375b25e4
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-21 19:53:10 +0300

    Add registration page with form in register.twig

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,6 @@ return static function(Router $router, ContainerInterface $container) {
     $router->map('GET', '/', \App\Http\Controllers\HomeController::class);
     $router->map('GET', '/about', \App\Http\Controllers\AboutController::class);
     $router->map('GET', '/users/{user}', \App\Http\Controllers\UserController::class);
+    $router->map('GET', '/auth/register', [\App\Http\Controllers\Auth\RegisterController::class, 'index']);
+    $router->map('POST', '/auth/register', [\App\Http\Controllers\Auth\RegisterController::class, 'store']);
 };
