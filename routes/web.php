31d02f337066495d3c42496903d9bcc827f1bc03
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-21 22:12:14 +0300

    Add CSRF middleware to web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use League\Route\Router;
 use Psr\Container\ContainerInterface;
 
 return static function(Router $router, ContainerInterface $container) {
+    $router->middleware($container->get('csrf'));
     $router->map('GET', '/', \App\Http\Controllers\HomeController::class);
     $router->map('GET', '/about', \App\Http\Controllers\AboutController::class);
     $router->map('GET', '/users/{user}', \App\Http\Controllers\UserController::class);
