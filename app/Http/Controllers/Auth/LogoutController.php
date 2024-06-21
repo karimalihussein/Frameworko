eb39cb0c6f4082dc02c84152e2ee811d14afd4f8
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-21 21:53:36 +0300

    Add LogoutController for handling user logout

---
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Http\Controllers\Auth;
+
+use Cartalyst\Sentinel\Sentinel;
+use Laminas\Diactoros\Response;
+use Psr\Http\Message\ServerRequestInterface;
+
+final class LogoutController
+{
+    public function __construct(
+        protected Sentinel $auth
+    ){}
+   
+    public function __invoke(ServerRequestInterface $request): Response
+    {
+        $this->auth->logout();
+        return new Response\RedirectResponse('/auth/login');
+    }
+}
