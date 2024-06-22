251c21377fff9a5e6fbbec5755d937192b89445d
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-22 09:00:33 +0300

    Add Symfony\Component\HttpFoundation\Session\Session dependency to LogoutController.php

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -6,16 +6,19 @@ namespace App\Http\Controllers\Auth;
 use Cartalyst\Sentinel\Sentinel;
 use Laminas\Diactoros\Response;
 use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 final class LogoutController
 {
     public function __construct(
-        protected Sentinel $auth
+        protected Sentinel $auth,
+        protected Session $session
     ){}
    
     public function __invoke(): Response
     {
         $this->auth->logout();
+        $this->session->getFlashBag()->add('success', 'You have been logged out');
         return new Response\RedirectResponse('/auth/login');
     }
 }
