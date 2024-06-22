4c31c27e5110ec3e31e51a82b51f3f8ee4bd6d04
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-22 09:00:30 +0300

    Refactor login functionality to display error messages and use CSRF protection

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,33 +1,41 @@
 <?php
 
-
 namespace App\Http\Controllers\Auth;
 
 use App\Views\View;
 use Cartalyst\Sentinel\Sentinel;
 use Laminas\Diactoros\Response;
 use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Laminas\Diactoros\Response\RedirectResponse;
 
 final class LoginController
 {
     public function __construct(
         protected View $view,
-        protected Sentinel $auth
+        protected Sentinel $auth,
+        protected Session $session
     ){}
+
     public function index(ServerRequestInterface $request): Response
     {
         $response = new Response();
-        $response->getBody()->write($this->view->render('auth/login'));
+        $response->getBody()->write($this->view->render('auth/login', [
+            'error' => $this->session->getFlashBag()->get('error'),
+            'success' => $this->session->getFlashBag()->get('success')
+        ]));
         return $response;
     }
 
-    public function store(ServerRequestInterface $request)
-    {        
-        if(!$this->auth->authenticate($request->getParsedBody()))
-        {
-            return new Response\RedirectResponse('/auth/login');
-        } 
+    public function store(ServerRequestInterface $request): Response
+    {
+        $credentials = $request->getParsedBody();
 
-        return new Response\RedirectResponse('/');
+        if (!$this->auth->authenticate($credentials)) {
+            $this->session->getFlashBag()->add('error', 'Invalid login credentials.');
+            return new RedirectResponse('/auth/login');
+        }
+
+        return new RedirectResponse('/');
     }
 }
