1baf5ad1afe21a28b60c8ed0e971a1eb4df42db4
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-23 11:05:31 +0300

    Refactor login functionality to use Respect\Validation for input validation and display error messages

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Config\Config;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 use League\Container\ServiceProvider\BootableServiceProviderInterface;
+use Respect\Validation\Factory;
 use Spatie\Ignition\Ignition;
 
 final class AppServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
@@ -14,6 +15,12 @@ final class AppServiceProvider extends AbstractServiceProvider implements Bootab
         if($this->getContainer()->get(Config::class)->get('app.debug')) {
             Ignition::make()->register();
         }
+
+        Factory::setDefaultInstance(
+            (new Factory())
+                ->withRuleNamespace('App\\Validation\\Rules')
+                ->withExceptionNamespace('App\\Validation\\Exceptions')
+        );
     }
 
     public function register(): void
