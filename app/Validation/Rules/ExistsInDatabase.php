eb6250f63519428ecfdf59ab5c1cdac82621defc
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-23 11:05:26 +0300

    Add ExistsInDatabase validation rule for database existence check

---
+++ app/Validation/Rules/ExistsInDatabase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Validation\Rules;
+
+use App\Core\Container;
+use Illuminate\Database\DatabaseManager;
+use Respect\Validation\Rules\AbstractRule;
+
+final class ExistsInDatabase extends AbstractRule
+{
+    public function __construct(
+        protected string $table,
+        protected string $column
+    ){}
+
+    public function validate($input): bool
+    {
+        return Container::getInstance()->get(DatabaseManager::class)
+            ->table($this->table)
+            ->where($this->column, $input)
+            ->exists();
+    }
+} 
