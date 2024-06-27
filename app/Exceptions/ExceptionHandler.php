138ed55aea27a5d97367edf02869225ac7952a68
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-27 04:50:45 +0300

    Add ExceptionHandler class to handle exceptions in the application

---
+++ app/Exceptions/ExceptionHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Exceptions;
+
+use App\Views\View;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Throwable;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+final class ExceptionHandler
+{
+    public function __construct(protected View $view) {}
+
+    /**
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws LoaderError
+     */
+    public function handle(ServerRequestInterface $request, ResponseInterface $response, Throwable $exception): ResponseInterface
+    {
+        $response->getBody()->write(
+            $this->view->render(
+                $this->getErrorView($exception),
+                ['exception' => $exception]
+            )
+        );
+        return $response;
+    }
+
+    protected function getErrorView(Throwable $exception): ?string
+    {
+        return match (get_class($exception)) {
+            \League\Route\Http\Exception\NotFoundException::class => 'errors/404',
+            default => 'errors/500',
+        };
+    }
+}
