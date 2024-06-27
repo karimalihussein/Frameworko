b07cf1464c621439ba0886401c514941f38a9040
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-27 03:25:53 +0300

    Refactor middleware in web.php to use RedirectIfGuestMiddleware and RedirectIfAuthenticatedMiddleware

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,12 @@ use Psr\Container\ContainerInterface;
 
 return static function(Router $router, ContainerInterface $container) {
     $router->middleware($container->get('csrf'));
-    $router->map('GET', '/', \App\Http\Controllers\HomeController::class)->middleware(new \App\Http\Middleware\ExampleMiddleware());
+    $router->map('GET', '/', \App\Http\Controllers\HomeController::class)->middleware(new \App\Http\Middleware\RedirectIfGuestMiddleware());
     $router->map('GET', '/about', \App\Http\Controllers\AboutController::class);
     $router->map('GET', '/users/{user}', \App\Http\Controllers\UserController::class);
-    $router->map('GET', '/auth/register', [\App\Http\Controllers\Auth\RegisterController::class, 'index']);
+    $router->map('GET', '/auth/register', [\App\Http\Controllers\Auth\RegisterController::class, 'index'])->middleware(new \App\Http\Middleware\RedirectIfAuthenticatedMiddleware());
     $router->map('POST', '/auth/register', [\App\Http\Controllers\Auth\RegisterController::class, 'store']);
-    $router->map('GET', '/auth/login', [\App\Http\Controllers\Auth\LoginController::class, 'index']);
+    $router->map('GET', '/auth/login', [\App\Http\Controllers\Auth\LoginController::class, 'index'])->middleware(new \App\Http\Middleware\RedirectIfAuthenticatedMiddleware());
     $router->map('POST', '/auth/login', [\App\Http\Controllers\Auth\LoginController::class, 'store']);
     $router->map('POST', '/auth/logout', App\Http\Controllers\Auth\LogoutController::class);
 };
