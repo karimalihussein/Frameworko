9b259248bc3436f9304bd3712b9fa5b93aa6f699
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-27 03:50:21 +0300

    Refactor web.php to use RouteGroup for authentication routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use League\Route\RouteGroup;
 use League\Route\Router;
 use Psr\Container\ContainerInterface;
 
@@ -9,9 +10,13 @@ return static function(Router $router, ContainerInterface $container) {
     $router->map('GET', '/', \App\Http\Controllers\HomeController::class)->middleware(new \App\Http\Middleware\RedirectIfGuestMiddleware());
     $router->map('GET', '/about', \App\Http\Controllers\AboutController::class);
     $router->map('GET', '/users/{user}', \App\Http\Controllers\UserController::class);
-    $router->map('GET', '/auth/register', [\App\Http\Controllers\Auth\RegisterController::class, 'index'])->middleware(new \App\Http\Middleware\RedirectIfAuthenticatedMiddleware());
-    $router->map('POST', '/auth/register', [\App\Http\Controllers\Auth\RegisterController::class, 'store']);
-    $router->map('GET', '/auth/login', [\App\Http\Controllers\Auth\LoginController::class, 'index'])->middleware(new \App\Http\Middleware\RedirectIfAuthenticatedMiddleware());
-    $router->map('POST', '/auth/login', [\App\Http\Controllers\Auth\LoginController::class, 'store']);
+
+    $router->group('/', function (RouteGroup $route) {
+        $route->map('GET', '/auth/register', [\App\Http\Controllers\Auth\RegisterController::class, 'index']);
+        $route->map('POST', '/auth/register', [\App\Http\Controllers\Auth\RegisterController::class, 'store']);
+        $route->map('GET', '/auth/login', [\App\Http\Controllers\Auth\LoginController::class, 'index']);
+        $route->map('POST', '/auth/login', [\App\Http\Controllers\Auth\LoginController::class, 'store']);
+    })->middleware(new \App\Http\Middleware\RedirectIfAuthenticatedMiddleware());
+
     $router->map('POST', '/auth/logout', App\Http\Controllers\Auth\LogoutController::class);
 };
