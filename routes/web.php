068efbcba55fb5d37d7777c402159df05f2a55b1
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-27 04:08:16 +0300

    Refactor authentication routes in web.php to use RouteGroup

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,20 @@ use League\Route\Router;
 use Psr\Container\ContainerInterface;
 
 return static function(Router $router, ContainerInterface $container) {
-    $router->middleware($container->get('csrf'));
-    $router->map('GET', '/', \App\Http\Controllers\HomeController::class)->middleware(new \App\Http\Middleware\RedirectIfGuestMiddleware());
-    $router->map('GET', '/about', \App\Http\Controllers\AboutController::class);
-    $router->map('GET', '/users/{user}', \App\Http\Controllers\UserController::class);
 
-    $router->group('/', function (RouteGroup $route) {
-        $route->map('GET', '/auth/register', [\App\Http\Controllers\Auth\RegisterController::class, 'index']);
-        $route->map('POST', '/auth/register', [\App\Http\Controllers\Auth\RegisterController::class, 'store']);
-        $route->map('GET', '/auth/login', [\App\Http\Controllers\Auth\LoginController::class, 'index']);
-        $route->map('POST', '/auth/login', [\App\Http\Controllers\Auth\LoginController::class, 'store']);
+    $router->middleware($container->get('csrf'));
+
+    $router->group('/auth', function (RouteGroup $route) {
+        $route->map('GET', '/register', [\App\Http\Controllers\Auth\RegisterController::class, 'index']);
+        $route->map('POST', '/register', [\App\Http\Controllers\Auth\RegisterController::class, 'store']);
+        $route->map('GET', '/login', [\App\Http\Controllers\Auth\LoginController::class, 'index']);
+        $route->map('POST', '/login', [\App\Http\Controllers\Auth\LoginController::class, 'store']);
     })->middleware(new \App\Http\Middleware\RedirectIfAuthenticatedMiddleware());
 
-    $router->map('POST', '/auth/logout', App\Http\Controllers\Auth\LogoutController::class);
+    $router->group('/', function (RouteGroup $route) {
+        $route->map('GET', '/', \App\Http\Controllers\HomeController::class);
+        $route->map('GET', '/about', \App\Http\Controllers\AboutController::class);
+        $route->map('GET', '/users/{user}', \App\Http\Controllers\UserController::class);
+        $route->map('POST', '/auth/logout', \App\Http\Controllers\Auth\LogoutController::class);
+    })->middleware(new \App\Http\Middleware\RedirectIfGuestMiddleware());
 };
