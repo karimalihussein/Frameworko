fa75482de0e7da7272ce9284dab7a8170fe94d16
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-06-27 03:11:45 +0300

    Refactor HomeController route to add ExampleMiddleware

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Psr\Container\ContainerInterface;
 
 return static function(Router $router, ContainerInterface $container) {
     $router->middleware($container->get('csrf'));
-    $router->map('GET', '/', \App\Http\Controllers\HomeController::class);
+    $router->map('GET', '/', \App\Http\Controllers\HomeController::class)->middleware(new \App\Http\Middleware\ExampleMiddleware());
     $router->map('GET', '/about', \App\Http\Controllers\AboutController::class);
     $router->map('GET', '/users/{user}', \App\Http\Controllers\UserController::class);
     $router->map('GET', '/auth/register', [\App\Http\Controllers\Auth\RegisterController::class, 'index']);
