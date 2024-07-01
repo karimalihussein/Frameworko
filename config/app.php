c6f0002919365947050f76d096a45d57335c5fb1
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-07-01 03:27:11 +0300

    Add PaginationServiceProvider to handle pagination functionality

--- config/app.php
+++ config/app.php
@@ -7,6 +7,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\RequestServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\PaginationServiceProvider::class,
         App\Providers\ViewServiceProvider::class,
         App\Providers\DatabaseServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
