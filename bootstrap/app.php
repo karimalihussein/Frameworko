ec19211ebe11b376879431cbf1f43af84db64c3c
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-07-01 03:09:44 +0300

    Refactor app.php to load environment variables and register service providers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,34 +2,32 @@
 
 use App\Providers\ConfigServiceProvider;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
-
-error_reporting(0);
-
-
 use App\Config\Config;
 use App\Core\Container;
 use League\Container\ReflectionContainer;
 use App\Core\App;
-
+use Dotenv\Dotenv;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+// Load environment variables
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
-
+// Initialize the container
 $container = Container::getInstance();
 $container->delegate(new ReflectionContainer());
 $container->addServiceProvider(new ConfigServiceProvider());
 
-
+// Load configuration
 $config = $container->get(Config::class);
 
-foreach($config->get('app.providers') as $provider) {
+// Register service providers
+foreach ($config->get('app.providers') as $provider) {
     $container->addServiceProvider(new $provider());
 }
 
-
+// Create and run the application
 $app = new App($container);
 (require __DIR__ . '/../routes/web.php')($app->getRouter(), $container);
 $app->run();
