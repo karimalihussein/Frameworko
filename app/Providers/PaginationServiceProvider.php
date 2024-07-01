9052ddb81fbdee9a685fc44e788e6220a8eba7eb
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-07-01 03:27:03 +0300

    Add PaginationServiceProvider to handle pagination functionality

---
+++ app/Providers/PaginationServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Pagination\Paginator;
+use Laminas\Diactoros\Request;
+use League\Container\ServiceProvider\AbstractServiceProvider;
+use League\Container\ServiceProvider\BootableServiceProviderInterface;
+
+final class PaginationServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
+{
+    public function boot(): void
+    {
+        Paginator::currentPathResolver(function () {
+            return (string) strtok($this->getContainer()->get(Request::class)->getUri(), '?');
+        });
+
+        Paginator::queryStringResolver(function () {
+            return $this->getContainer()->get(Request::class)->getQueryParams();
+        });
+
+        Paginator::currentPageResolver(function ($pageName = 'page') {
+            return $this->getContainer()->get(Request::class)->getQueryParams()[$pageName] ?? 1;
+        });
+    }
+
+    public function register(): void
+    {
+    }
+
+    public function provides(string $id): bool
+    {
+        $services = [];
+
+        return in_array($id, $services);
+    }
+}
