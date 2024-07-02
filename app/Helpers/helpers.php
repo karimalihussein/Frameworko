f6c60cdc53b32c66a1dfda32506c58ea15556838
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-07-02 06:46:11 +0300

    Refactor helpers.php to use app helper function for view rendering

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,8 +1,14 @@
 <?php
 
 use App\Core\Container;
+use App\Views\View;
 
 function app(string $abstract)
 {
     return Container::getInstance()->get($abstract);
+}
+
+function view(string $view, array $data = [])
+{
+    return app(View::class)->render($view, $data);
 }
