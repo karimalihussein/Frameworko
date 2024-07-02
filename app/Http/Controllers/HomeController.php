cc4351f1fff51f80916ed0ab9ec7fb96b607ecd4
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-07-02 06:46:14 +0300

    Refactor HomeController to use Laravel's view helper function for rendering the home view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,30 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Config\Config;
 use App\Models\User;
-use App\Views\View;
-use Cartalyst\Sentinel\Sentinel;
-use Illuminate\Database\DatabaseManager;
-use Laminas\Diactoros\Request;
 use Laminas\Diactoros\Response;
-use Psr\Http\Message\ServerRequestInterface;
-use Symfony\Component\HttpFoundation\Session\Session;
 
 final class HomeController
 {
     public function __construct(
-        protected View $view,
-        protected Sentinel $auth,
-        protected Session $session,
-    ){}
+    ) {
+    }
 
-    public function __invoke(ServerRequestInterface $request): Response
+    public function __invoke(): Response
     {
         $response = new Response();
-        $response->getBody()->write($this->view->render('home', [
-            'users' =>  User::get()
-        ]));
+        $response->getBody()->write(
+            view('home', [
+                'users' =>  User::get()
+            ])
+        );
         return $response;
     }
 }
