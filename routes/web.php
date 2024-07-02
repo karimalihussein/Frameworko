bac11fd3ead300bc7816875cc3f4855887834e91
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-07-02 07:26:24 +0300

    Refactor route paths in web.php to use named routes and add route helper function to TwigExtension.php

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,16 @@ return static function(Router $router, ContainerInterface $container) {
     $router->middleware(new \App\Http\Middleware\FlashOldDataMiddleware());
 
     $router->group('/auth', function (RouteGroup $route) {
-        $route->map('GET', '/register', [\App\Http\Controllers\Auth\RegisterController::class, 'index']);
-        $route->map('POST', '/register', [\App\Http\Controllers\Auth\RegisterController::class, 'store']);
-        $route->map('GET', '/login', [\App\Http\Controllers\Auth\LoginController::class, 'index']);
-        $route->map('POST', '/login', [\App\Http\Controllers\Auth\LoginController::class, 'store']);
+        $route->map('GET', '/register', [\App\Http\Controllers\Auth\RegisterController::class, 'index'])->setName('register');
+        $route->map('POST', '/register', [\App\Http\Controllers\Auth\RegisterController::class, 'store'])->setName('register.store');
+        $route->map('GET', '/login', [\App\Http\Controllers\Auth\LoginController::class, 'index'])->setName('login');
+        $route->map('POST', '/login', [\App\Http\Controllers\Auth\LoginController::class, 'store'])->setName('login.store');
     })->middleware(new \App\Http\Middleware\RedirectIfAuthenticatedMiddleware());
 
     $router->group('/', function (RouteGroup $route) {
         $route->map('GET', '/', \App\Http\Controllers\HomeController::class)->setName('home');
         $route->map('GET', '/users', [\App\Http\Controllers\UserController::class, 'index'])->setName('users');
-        $route->map('GET', '/users/{user}', [\App\Http\Controllers\UserController::class, 'show']);
+        $route->map('GET', '/users/{user}', [\App\Http\Controllers\UserController::class, 'show'])->setName('users.show');
         $route->map('POST', '/auth/logout', \App\Http\Controllers\Auth\LogoutController::class)->setName('logout');
     })->middleware(new \App\Http\Middleware\RedirectIfGuestMiddleware());
 
