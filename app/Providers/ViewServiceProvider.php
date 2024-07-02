fda9a4b8f2396aad2a803e201e89c810ba69a8f8
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-07-02 06:50:26 +0300

    Refactor ViewServiceProvider to use config helper function for app debug retrieval

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -19,11 +19,10 @@ final class ViewServiceProvider extends AbstractServiceProvider implements Boota
     {
         $this->getContainer()->add(View::class, function(){
             $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../../resources/views');
-            $isDebugEnabled = $this->getContainer()->get(Config::class)->get('app.debug');
             
             $twig = new \Twig\Environment($loader, [
                 'cache' => false,
-                'debug' => $isDebugEnabled
+                'debug' => config('app.debug'),
             ]);
 
             $twig->addRuntimeLoader(new TwigRuntimeLoader($this->getContainer()));
